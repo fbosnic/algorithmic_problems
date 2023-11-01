e5e4a0f50b1a4879915260ee9da643105aa7a3c2
Filip <filip.boss1@hotmail.com>
2023-11-01 15:38:36 +0000

    Close to first version of "how many substrings"

--- how_many_substrings/src/main.rs
+++ how_many_substrings/src/main.rs
@@ -72,27 +72,65 @@ impl<T: Add<Output = T> + AddAssign + Copy + Default + Neg<Output = T> + Mul<i32
     }
 }
 
+fn _update_prefix_counter_with(fw_counter: &mut AdvanceFenwickTree<i32>, left_len: usize, right_len: usize, comon_prefix: usize, value: i32) {
+    let min_len = min(left_len, right_len);
+    fw_counter.add(min_len - comon_prefix, min_len, value);
+}
 
 fn count_substrings(str: String, queries: Vec<Range<usize> >) -> Vec<i32> {
     let n = str.len();
-    let suffix_array = algorithms::string::generate_suffix_array_manber_myers(&str);
-    let sa_lookup = inverse_permutation(&suffix_array);
-    let lcp = lcp_construction(str.as_bytes(), suffix_array);
+    let sa = algorithms::string::generate_suffix_array_manber_myers(&str);
+    let sa_lookup = inverse_permutation(&sa);
+    let lcp = lcp_construction(str.as_bytes(), &sa);
     let min_lcp_segment_tree = algorithms::data_structures::SegmentTree::from_vec(&lcp, min);
 
     let mut suffix_sorter: RBTree<usize, i8> = RBTree::new();
-    let fw_substr_count : FenwickTree<u32> = FenwickTree::with_len(n);
-    let fw_lcp_count: FenwickTree<u32> = FenwickTree::with_len(n);
+    let fw_prefix_counter: AdvanceFenwickTree<i32> = AdvanceFenwickTree::with_len(n);
+    fw_prefix_counter.add(0, 1, i32::try_from(lcp.iter().sum()).unwrap());
 
-    for suffix in (0..n-1).rev() {
+    for suffix in (0..n).rev() {
         let sa_pos = sa_lookup[suffix];
         suffix_sorter.insert(sa_pos, 0);
-        let node = suffix_sorter.find_node(&sa_pos);
-        if let Some(i) = node {
-            println!("{}", i.key);
+        let node = suffix_sorter.find_node(&sa_pos).unwrap();
+
+        let prev_node = suffix_sorter.previous_by_key(node);
+        let next_node = suffix_sorter.next_by_key(node);
+
+        if !prev_node.is_none() && !next_node.is_none() {
+            let sa_left = prev_node.unwrap().key;
+            let sa_right = next_node.unwrap().key;
+            let cp = min_lcp_segment_tree.query(sa_left..sa_right).unwrap();
+            _update_prefix_counter_with(
+                &mut fw_prefix_counter,
+                sa[sa_left],
+                sa[sa_right],
+                cp,
+                1
+            );
+        }
+        if !prev_node.is_none() {
+            let sa_left = prev_node.unwrap().key;
+            let cp = min_lcp_segment_tree.query(sa_left..sa_pos).unwrap();
+            _update_prefix_counter_with(
+                &mut fw_prefix_counter,
+                sa[sa_left],
+                sa[sa_pos],
+                cp,
+                -1
+            );
+        }
+        if !next_node.is_none() {
+            let sa_right = next_node.unwrap().key;
+            let cp = min_lcp_segment_tree.query(sa_pos..sa_right).unwrap();
+            _update_prefix_counter_with(
+                &mut fw_prefix_counter,
+                sa[sa_pos],
+                sa[sa_right],
+                cp,
+                -1
+            );
         }
     }
-    println!("{lcp:?}");
     return vec![1, 2, 3]
 }
 
